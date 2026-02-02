9d69ea3d22bc032e3e481143bd6e9f99ada1b304
PraveenAIT <praveendevan1508@gmail.com>
2026-02-02 21:37:51 +0530

    Refactor demoCamera to esp32ble with Bluetooth features

--- main.ts
+++ main.ts
@@ -1,18 +1,42 @@
 /**
- * Demo LED Extension
+ * ESP32 BLE UART Extension for micro:bit
  */
 
-//% color=#ff6600 icon="\uf111" block="Demo Camera"
-namespace demoCamera {
+//% color=#0fbc11 icon="\uf2db" block="ESP32 BLE"
+namespace esp32ble {
 
-    //% block="camera detected circle"
-    export function cameraDetectedCircle(): void {
-        basic.showIcon(IconNames.Target)
+    let lastMessage = ""
+
+    /**
+     * Initialize Bluetooth UART service
+     */
+    //% block="ESP32 BLE init"
+    export function init(): void {
+        bluetooth.startUartService()
     }
 
-    //% block="camera detected nothing"
-    export function cameraDetectedNothing(): void {
-        basic.clearScreen()
+    /**
+     * Read message from ESP32 (expects newline \\n)
+     */
+    //% block="ESP32 read message"
+    export function readMessage(): string {
+        lastMessage = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+        return lastMessage
     }
 
+    /**
+     * Check if last message contains given text
+     */
+    //% block="ESP32 message contains %text"
+    export function messageContains(text: string): boolean {
+        return lastMessage.indexOf(text) >= 0
+    }
+
+    /**
+     * Get last received message
+     */
+    //% block="ESP32 last message"
+    export function last(): string {
+        return lastMessage
+    }
 }
